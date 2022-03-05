41da3c03c7f8185ed0568d44e4874f6f60f9e6ab
yaqoubalghadban <yoyob4654@gmail.com>
2022-03-05 13:35:29 +0300

    done

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var name="yaqoub"
+var hobby="diving"
+var favNumber=99
+print("hi my name is \(name) and my hobby is \(hobby),my favourite number is \(favNumber)")
+var age=18
+var hight=1.9
